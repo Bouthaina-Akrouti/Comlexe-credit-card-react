39372d51fbb13eda97f62f725a2c0b4dc69dcb14
bothazerty12 <bouthaynaakrouti522@gmail.com>
2019-11-04 11:00:19 +0100

    Updates

---
+++ precache-manifest.35378138b1efb61bc3cc0ee5698ab201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8461f2d62167efc6fbe8ccaa52e66cba",
+    "url": "/Comlexe-credit-card-react/index.html"
+  },
+  {
+    "revision": "380ebd9736a36be68099",
+    "url": "/Comlexe-credit-card-react/static/css/main.59cd7412.chunk.css"
+  },
+  {
+    "revision": "ed9a52d3d2983c5f647f",
+    "url": "/Comlexe-credit-card-react/static/js/2.23692ef1.chunk.js"
+  },
+  {
+    "revision": "380ebd9736a36be68099",
+    "url": "/Comlexe-credit-card-react/static/js/main.3ed645aa.chunk.js"
+  },
+  {
+    "revision": "8a134c9d59ede0051b78",
+    "url": "/Comlexe-credit-card-react/static/js/runtime-main.52471ec8.js"
+  }
+]);
